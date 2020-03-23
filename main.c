a9cf327815d86c6c52ef61d4feae622b8412b252
Daniyal Marofi <daniyaleight8@gmail.com>
2020-03-24 02:22:50 +0430

    add daniCheck

--- main.c
+++ main.c
@@ -42,148 +42,124 @@ int main(){
     return 0;
 }
 
-int checkFirstVertical(int row,int col, char turn){
+int daniCheck(char action, int row,int col, char turn){
+    // action::
+    // v = top down check
+    // V = bottom up check
+    // h = left right check
+    // H = right left check
+    // 1 = first diaginal check
+    // 2 = second diagonal check
+    // 3 = third diagonal check
+    // 4 = forth diagonal check
     int i;
     for (i = 0; i < 4; i++){
-        if(board[row+i][col]!=turn){
-            return FALSE;
+        switch (action){
+            case 'v':
+            if(board[row+i][col]!=turn) return FALSE;
+            break;
+            case 'V':
+            if(board[row-i][col]!=turn) return FALSE;
+            break;
+            case 'h':
+            if(board[row][col+i]!=turn) return FALSE;
+            break;
+            case 'H':
+            if(board[row][col-i]!=turn) return FALSE;
+            break;
+            case '1':
+            if(board[row+i][col+i]!=turn) return FALSE;
+            break;
+            case '2':
+            if(board[row+i][col-i]!=turn) return FALSE;
+            break;
+            case '3':
+            if(board[row-i][col+i]!=turn) return FALSE;
+            break;
+            case '4':
+            if(board[row-i][col-i]!=turn) return FALSE;
+            break;
         }
     }
-    if(board[row+3][col+1]==turn || board[row+3][col-1]==turn){
-        return TRUE;
-    }else{
-        return FALSE;
-    }
-}
+    int row1,col1,row2,col2;
 
-int checkSecondVertical(int row,int col, char turn){
-    int i;
-    for (i = 0; i < 4; i++){
-        if(board[row-i][col]!=turn){
-            return FALSE;
-        }
+    switch (action){
+        case 'v':
+        row1=3;col1=1;
+        row2=3;col2=-1;
+        break;
+        case 'V':
+        row1=-3;col1=1;
+        row2=-3;col2=-1;
+        break;
+        case 'h':
+        row1=1;col1=3;
+        row2=-1;col2=3;
+        break;
+        case 'H':
+        row1=1;col1=-3;
+        row2=-1;col2=-3;
+        break;
+        case '1':
+        row1=4;col1=2;
+        row2=2;col2=4;
+        break;
+        case '2':
+        row1=4;col1=-2;
+        row2=2;col2=-4;
+        break;
+        case '3':
+        row1=-4;col1=2;
+        row2=-2;col2=4;
+        break;
+        case '4':
+        row1=-2;col1=-4;
+        row2=-4;col2=-2;
+        break;
     }
-    if(board[row-3][col+1]==turn || board[row-3][col-1]==turn){
-        return TRUE;
-    }else{
-        return FALSE;
-    }
-}
 
-int checkFirstHorizontal(int row,int col, char turn){
-    int i;
-    for (i = 0; i < 4; i++){
-        if(board[row][col+i]!=turn){
-            return FALSE;
-        }
-    }
-    if(board[row+1][col+3]==turn || board[row-1][col+3]==turn){
+    if(board[row+row1][col+col1]==turn || board[row+row2][col+col2]==turn){
         return TRUE;
-    }else{
-        return FALSE;
     }
-}
-
-int checkSecondHorizontal(int row,int col, char turn){
-    int i;
-    for (i = 0; i < 4; i++){
-        if(board[row][col-i]!=turn){
-            return FALSE;
-        }
-    }
-    if(board[row+1][col-3]==turn || board[row-1][col-3]==turn){
-        return TRUE;
-    }else{
-        return FALSE;
-    }
-}
-
-
-int checkFirstDiagonal(int row,int col, char turn){
-    int i;
-    for (i = 0; i < 4; i++){
-        if(board[row+i][col+i]!=turn){
-            return FALSE;
-        }
-    }
-    if(board[row+4][col+2]==turn || board[row+2][col+4]==turn){
-        return TRUE;
-    }else{
-        return FALSE;
-    }
-}
-
-int checkSecondDiagonal(int row,int col, char turn){
-    int i;
-    for (i = 0; i < 4; i++){
-        if(board[row+i][col-i]!=turn){
-            return FALSE;
-        }
-    }
-    if(board[row+4][col-2]==turn || board[row+2][col-4]==turn){
-        return TRUE;
-    }else{
-        return FALSE;
-    }
-}
-
-int checkThirdDiagonal(int row,int col, char turn){
-    int i;
-    for (i = 0; i < 4; i++){
-        if(board[row-i][col+i]!=turn){
-            return FALSE;
-        }
-    }
-    if(board[row-4][col+2]==turn || board[row-2][col+4]==turn){
-        return TRUE;
-    }else{
-        return FALSE;
-    }
-}
-
-int checkForthDiagonal(int row,int col, char turn){
-    int i;
-    for (i = 0; i < 4; i++){
-        if(board[row-i][col-i]!=turn){
-            return FALSE;
-        }
-    }
-    if(board[row-2][col-4]==turn || board[row-4][col-2]==turn){
-        return TRUE;
-    }else{
+    else{
         return FALSE;
     }
 }
 
 char checkWinner(){
+
     for (int i = 0; i < 6; i++){
         for (int j = 0; j < 6; j++){
+
             if(j<3){
-                if(checkFirstHorizontal(i,j,'b')){return 'b';}
-                if(checkFirstHorizontal(i,j,'w')){return 'w';}
+                if(daniCheck('h',i,j,'b')) return 'b';
+                if(daniCheck('h',i,j,'w')) return 'w';
             }else{
-                if(checkSecondHorizontal(i,j,'b')){return 'b';}
-                if(checkSecondHorizontal(i,j,'w')){return 'w';}
+                if(daniCheck('H',i,j,'b')) return 'b';
+                if(daniCheck('H',i,j,'w')) return 'w';
             }
+
             if(i<3){
-                if(checkFirstVertical(i,j,'b')){return 'b';}
-                if(checkFirstVertical(i,j,'w')){return 'w';}
+                if(daniCheck('v',i,j,'b')) return 'b';
+                if(daniCheck('v',i,j,'w')) return 'w';
+
                 if(j<3){
-                    if(checkFirstDiagonal(i,j,'b')){return 'b';}
-                    if(checkFirstDiagonal(i,j,'w')){return 'w';}
+                    if(daniCheck('1',i,j,'b')) return 'b';
+                    if(daniCheck('1',i,j,'w')) return 'w';
                 }else{
-                    if(checkSecondDiagonal(i,j,'b')){return 'b';}
-                    if(checkSecondDiagonal(i,j,'w')){return 'w';}
+                    if(daniCheck('2',i,j,'b')) return 'b';
+                    if(daniCheck('2',i,j,'w')) return 'w';
                 }
             }else{
-                if(checkSecondVertical(i,j,'b')){return 'b';}
-                if(checkSecondVertical(i,j,'w')){return 'w';}
+                if(daniCheck('V',i,j,'b')) return 'b';
+                if(daniCheck('V',i,j,'w')) return 'w';
+                
                 if(j<3){
-                    if(checkThirdDiagonal(i,j,'b')){return 'b';}
-                    if(checkThirdDiagonal(i,j,'w')){return 'w';}
+                    if(daniCheck('3',i,j,'b')) return 'b';
+                    if(daniCheck('3',i,j,'w')) return 'w';
                 }else{
-                    if(checkForthDiagonal(i,j,'b')){return 'b';}
-                    if(checkForthDiagonal(i,j,'w')){return 'w';}
+                    if(daniCheck('4',i,j,'b')) return 'b';
+                    if(daniCheck('4',i,j,'w')) return 'w';
                 }
             }
         }
